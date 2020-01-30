663f24fc38c331cc88a5533b826bcb48c1a6362e
Mansi Kathuria <kmansi310@gmail.com>
2020-01-30 19:33:42 +0530

    first_test

---
+++ client/src/components/Join.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import {Link} from 'react-router-dom';
+
+const Join = () => {
+//  const [name, setName] = useState('');
+//  const [room, setRoom] = useState('');
+
+  return (
+     <div className="joinOuterContainer">
+      hello react
+     </div>
+  )
+};
+
+export default Join;
